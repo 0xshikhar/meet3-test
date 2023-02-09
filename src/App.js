067e426e715195929f47a2911379d6197beb33b9
linux369 <shikhar@iiitmanipur.ac.in>
2023-02-10 02:40:41 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Home from './home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Match from './match'
+import Meeting from './meeting'
+
+
+
+function App() {
+
+  
+  return (
+  
+   
+       
+      
+        <Home />
+
+            
+   
+   
+  );
+}
+
+export default App;
